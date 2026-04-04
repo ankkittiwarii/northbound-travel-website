3db7cd1ed3e981ec3ffeb9df5aa6541f2649db14
Ankkit-Tiwarii <tiwariankit1062@gmail.com>
2026-04-04 11:05:06 +0530

    database update

--- database/travel.sql
+++ database/travel.sql
@@ -2,38 +2,47 @@ CREATE DATABASE travel;
 
 USE travel;
 
+
 CREATE TABLE users (
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(100),
-email VARCHAR(100) UNIQUE,
-password VARCHAR(255),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE bookings (
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(100),
-email VARCHAR(100),
-phone VARCHAR(20),
-destination VARCHAR(100),
-persons INT,
-travel_date DATE,
-message TEXT,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    destination VARCHAR(100),
+    persons INT,
+    travel_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE contact(
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(100),
-email VARCHAR(100),
-message TEXT
+
+CREATE TABLE contact (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    message TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
 
 CREATE TABLE admin (
-id INT AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(100),
-password VARCHAR(255)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    password VARCHAR(255)
 );
+
 
 INSERT INTO admin (username,password)
 VALUES ('admin', MD5('admin123'));
